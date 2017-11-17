ba93fabd480920848d36b9af8ef2d676897eb19a
Luan Pham <luanpm88@gmail.com>
2017-11-17 00:20:33 -0800

    default state

--- app/controllers/erp/consignments/backend/consignment_details_controller.rb
+++ app/controllers/erp/consignments/backend/consignment_details_controller.rb
@@ -5,7 +5,9 @@ module Erp
         def consignment_detail_line_form
           @consignment_detail = ConsignmentDetail.new
           @consignment_detail.product_id = params[:add_value]
-          
+
+          @consignment_detail.state = Erp::Products::State.first
+
           render partial: params[:partial], locals: {
             consignment_detail: @consignment_detail,
             uid: helpers.unique_id()
